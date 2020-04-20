e1e80ce14ee4a2676104ca673fd1dcc1d10efd5c
Rahul Pandey <hello@rkpandey.com>
2020-04-19 18:14:22 -0700

    Add a button

--- app/src/main/java/edu/stanford/rkpandey/githubdemo1/MainActivity.kt
+++ app/src/main/java/edu/stanford/rkpandey/githubdemo1/MainActivity.kt
@@ -2,11 +2,15 @@ package edu.stanford.rkpandey.githubdemo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            tvHello.text = "Look at me!!"
+        }
     }
 }
